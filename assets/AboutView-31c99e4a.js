b39c89b2ea4d0d426d9ef2b7a144d5f74464ea19
derpmagician <renzin@gmail.com>
2023-10-09 16:50:34 -0500

    Updates

---
+++ assets/AboutView-31c99e4a.js
@@ -0,0 +1,1 @@
+import{o as _,c as n,b as a}from"./index-80471d30.js";const r=(e,t)=>{const o=e.__vccOpts||e;for(const[s,c]of t)o[s]=c;return o};const i={},u={class:"about"},l=a("h1",null,"This is an about page",-1),d=[l];function f(e,t){return _(),n("div",u,d)}const h=r(i,[["render",f]]);export{h as default};
