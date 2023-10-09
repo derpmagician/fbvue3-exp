184324f9a3b20088a83487a8eddfdd8b281fc5ca
derpmagician <renzin@gmail.com>
2023-10-09 17:16:35 -0500

    diferente build

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: import.meta.env.MODE === 'production' ? '/fbvue3-exp/' : './',
+  base: process.env.NODE_ENV === 'production' ? '/fbvue3-exp/' : './',
   minify: false,
   plugins: [
     vue(),
