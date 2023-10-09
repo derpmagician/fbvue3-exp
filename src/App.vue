3a058f2e225b911f79ff51eb9a2eec8c6cdd1015
derpmagician <renzin@gmail.com>
2023-10-09 14:50:58 -0500

    cambio de componente

--- src/App.vue
+++ src/App.vue
@@ -2,34 +2,34 @@
 import { RouterLink, RouterView } from 'vue-router';
 import { useDark, useToggle } from '@vueuse/core';
 import { useUserStore } from "./stores/user";
-import CustomHeader from './components/CustomHeader.vue';
+// import CustomHeader from './components/CustomHeader.vue';
 
-const userStore = useUserStore();
+// const userStore = useUserStore();
 
-const isDark = useDark(
-  {
-    selector: 'body',
-    // attribute: 'b-dark',
-    valueDark: 'bd-dark',
-    valueLight: 'bd-light',
-  },
-  {
-    selector: 'a',
-    // attribute: 'b-dark',
-    valueDark: 'bd-dark',
-    valueLight: 'bd-light',
-  },
+// const isDark = useDark(
+//   {
+//     selector: 'body',
+//     // attribute: 'b-dark',
+//     valueDark: 'bd-dark',
+//     valueLight: 'bd-light',
+//   },
+//   {
+//     selector: 'a',
+//     // attribute: 'b-dark',
+//     valueDark: 'bd-dark',
+//     valueLight: 'bd-light',
+//   },
 
-  )
-const toggleDark = useToggle(isDark)
+//   )
+// const toggleDark = useToggle(isDark)
 
 </script>
 
 <template>
-<CustomHeader :isDark="isDark" @toggleDark="toggleDark"
-  class="container my-2 p-4" :class="isDark ? 'bg-dark' : 'bg-light'"/>
+<!-- <CustomHeader :isDark="isDark" @toggleDark="toggleDark"
+  class="container my-2 p-4" :class="isDark ? 'bg-dark' : 'bg-light'"/> -->
 <div>Veamos</div>
-<div>{{userStore}}</div>
+<!-- <div>{{userStore}}</div> -->
 
 <div class="container-fluid">
 
