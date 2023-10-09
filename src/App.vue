d9e285c603eda38b0ebe44e69ed824c71dad34d5
derpmagician <renzin@gmail.com>
2023-10-09 17:07:18 -0500

    diferente build

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,7 @@ const toggleDark = useToggle(isDark)
 <template>
 <CustomHeader :isDark="isDark" @toggleDark="toggleDark"
   class="container my-2 p-4" :class="isDark ? 'bg-dark' : 'bg-light'"/>
-<div>Veamos 4.1</div>
+<div>Veamos 1</div>
 <!-- <div>{{userStore}}</div> -->
 
 <div class="container-fluid">
