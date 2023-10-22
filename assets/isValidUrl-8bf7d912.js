994342979df514a66ffd4f57a8b7b6a8d910b769
derpmagician <renzin@gmail.com>
2023-10-22 10:43:18 -0500

    Updates

---
+++ assets/isValidUrl-8bf7d912.js
@@ -0,0 +1,1 @@
+import{p as r}from"./index-f9c76e6e.js";const a=t=>({isValidUrl:r(()=>/^(http|https):\/\/[^ "]+$/.test(t.value))});export{a as u};
--- assets/isValidUrl-d3f578a2.js
+++
@@ -1,1 +0,0 @@
-import{K as r}from"./index-c9b0f481.js";const a=t=>({isValidUrl:r(()=>/^(http|https):\/\/[^ "]+$/.test(t.value))});export{a as u};
