cd2547c569fba4792683df5683b2887bc4d4e475
derpmagician <renzin@gmail.com>
2023-10-24 15:35:13 -0500

    Updates

--- assets/isValidUrl-480ccde7.js
+++
@@ -1,1 +0,0 @@
-import{q as r}from"./index-580d3cbb.js";const a=t=>({isValidUrl:r(()=>/^(http|https):\/\/[^ "]+$/.test(t.value))});export{a as u};
---
+++ assets/isValidUrl-b38ef2f4.js
@@ -0,0 +1,1 @@
+import{q as r}from"./index-4c9da3b4.js";const a=t=>({isValidUrl:r(()=>/^(http|https):\/\/[^ "]+$/.test(t.value))});export{a as u};
